e018f61ae766ab3b7f2b4533ba201ac0f8b2f4cb
kensidiangco <kensidiangco@gmail.com>
2021-09-25 21:31:48 +0800

    make it betterrr

--- constants/Utils.tsx
+++ constants/Utils.tsx
@@ -6,9 +6,6 @@ export namespace JwtUtils {
     const currentTime = Math.round(Date.now() / 1000 + 60);
     const decoded = jwt.decode(token);
 
-    console.log(`Current time + 60 seconds: ${new Date(currentTime * 1000)}`);
-    console.log(`Token lifetime: ${new Date(decoded["exp"] * 1000)}`);
-
     if (decoded["exp"]) {
       const adjustedExpiry = decoded["exp"];
 

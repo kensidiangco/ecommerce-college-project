f79d5908c694614b14c444731e8b9a09166b7412
kensidiangco <kensidiangco@gmail.com>
2021-09-29 04:02:35 +0800

    make if better than before you see it

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
   env: {
-    BACKEND_API_BASE: 'https://dailylife-api.herokuapp.com/',
+    BACKEND_API_BASE: 'https://dailylife-api.herokuapp.com',
     IMAGE_BASE: 'https://res.cloudinary.com/dailylife-ecommerce'
   },
 }

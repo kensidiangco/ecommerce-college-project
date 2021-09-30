a2c7419a2b5c68bdfca15a1e4791469e0135a748
kensidiangco <kensidiangco@gmail.com>
2021-10-01 04:51:57 +0800

    fixing layout bug

--- components/checkOutItem.tsx
+++ components/checkOutItem.tsx
@@ -1,38 +1,38 @@
 import Image from 'next/image'
 import NumberWithSpace from './currency';
-import { useState, useEffect } from 'react';
+import Link from 'next/link'
 
-export default function CheckOutItem({ id, title, price, quantity, product_image, image, variation, slug }) {
-    const [photo, setPhoto] = useState('')
+export default function CheckOutItem({ id, title, price, quantity, photo, variation, slug }) {
 	const variations = Array.isArray(variation) && variation.length ? variation[0] : {};
 	const headers = Object.keys(variations)
 	const body = Object.values(variations)
-
-    useEffect(() => {
-        if(image){
-            setPhoto(`${image}`)
-        }else{
-            setPhoto(`${product_image[0].image}`)
-        }
-    }, [])
     
     return (
-        <div className="flex flex-row gap-2 md:gap-4 items-center">
-            <Image src={`${process.env.IMAGE_BASE}/${photo}`} width={130} height={130} objectFit="contain" alt={title}/>
-            <div className="flex flex-col gap-px">
+        <div className="flex md:flex-row flex-col gap-2 dark:bg-dark-card rounded-md">
+            <Link href={`/dailylife/product/${slug}`} passHref>
+                <Image src={`${photo}`} width={200} height={200} alt={title} className="rounded-md cursor-pointer"/>
+            </Link>
+            <div className="flex flex-col px-4 py-2 md:px-0">
                 <p className="text-md font-semibold text-gray-600 dark:text-gray-50 transition delay-100">{title}</p>
                 <p className="text-sm text-gray-700 dark:text-gray-50 transition delay-100">
-                {headers.map((header, i) => (
-                    <th key={i}>{header}&nbsp;&nbsp;&nbsp;&nbsp;</th>
-                ))}
+                    {headers.map((header, i) => (
+                        <th key={i}>{header}&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                    ))}
                 </p>
                 <p className="text-sm text-gray-700 dark:text-gray-50 transition delay-100">
-                {body.map((value, i) => (
-                    <td key={i}>{value}&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                ))}
+                    {body.map((value, i) => (
+                        <td key={i}>{value}&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                    ))}
                 </p>
-                <p className="text-md text-blue-700 dark:text-blue-400 transition delay-100">{NumberWithSpace(price)} <span className="text-sm text-gray-500 dark:text-gray-400 transition delay-100">{quantity > 1 ?  <>{quantity}pcs</> : <>{quantity}pc</>}</span></p>
-                <p className="text-sm md:py-2 text-yellow-700 dark:text-yellow-300 transition delay-100">Subtotal: {NumberWithSpace(price * quantity)}</p>
+                <p className="text-md text-blue-700 dark:text-blue-400 transition delay-100">
+                    {NumberWithSpace(price)} 
+                    <span className="text-sm text-gray-500 dark:text-gray-400 transition delay-100">
+                            {quantity > 1 ?  <>{quantity}pcs</> : <>{quantity}pc</>}
+                        </span>
+                    </p>
+                <p className="text-sm md:py-2 text-yellow-700 dark:text-yellow-300 transition delay-100">
+                    Subtotal: {NumberWithSpace(price * quantity)}
+                </p>
             </div>
         </div>
     )
--- pages/dailylife/checkout.tsx
+++ pages/dailylife/checkout.tsx
@@ -24,39 +24,41 @@ function Checkout() {
 			<Head>
 				<title>Checkout</title>
 			</Head>
-			<div className="container mx-auto">
-				<div className="flex flex-col-reverse md:flex-row justify-center p-4 md:px-5 md:py-10 md:gap-10 gap-5">
-					<form onSubmit={onSubmit} className="flex flex-col gap-2 p-5 rounded-xl shadow-md bg-gray-200 dark:bg-dark-card text-gray-800 dark:text-gray-50" autoComplete="off">
-						<h2 className="text-xl font-semibold">Information</h2>
-						<input type="email" name="email" placeholder="Email" defaultValue={!loading? session.user?.email : ""} className="p-2 rounded-md" required/>
+			<div className="container mx-auto px-5">
+				<div className="flex flex-col md:flex-row justify-center p-4 md:px-5 md:py-10 md:gap-8 gap-5">
+					<form onSubmit={onSubmit} autoComplete="off">
+						<div className="flex flex-col gap-2 p-5 rounded-md shadow-md bg-gray-200 dark:bg-dark-card text-gray-800 dark:text-gray-50">
+							<h2 className="text-xl font-semibold">Information</h2>
+							<input type="email" name="email" placeholder="Email" defaultValue={!loading? session.user?.email : ""} className="p-2 rounded-md" required/>
 
-						<h2 className="mt-2 text-xl font-semibold">Delivery address</h2>
-						<input type="text" name="name" placeholder="Full name" className="p-2 rounded-md" required/>
-						<input type="text" name="address1" placeholder="Present address" className="p-2 rounded-md" required/>
-						<input type="text" name="brgy" placeholder="Baranggay" className="p-2 rounded-md" required/>
-						<input type="text" name="zone" placeholder="Zone" className="p-2 rounded-md" required/>
-						<input type="text" name="district" placeholder="District" className="p-2 rounded-md" required/>
-						<input type="text" name="zip code" placeholder="Zip code" className="p-2 rounded-md" required/>
+							<h2 className="mt-2 text-xl font-semibold">Delivery address</h2>
+							<input type="text" name="name" placeholder="Full name" className="p-2 rounded-md" required/>
+							<input type="text" name="address1" placeholder="Present address" className="p-2 rounded-md" required/>
+							<input type="text" name="brgy" placeholder="Baranggay" className="p-2 rounded-md" required/>
+							<input type="text" name="zone" placeholder="Zone" className="p-2 rounded-md" required/>
+							<input type="text" name="district" placeholder="District" className="p-2 rounded-md" required/>
+							<input type="text" name="zip code" placeholder="Zip code" className="p-2 rounded-md" required/>
 
-						<h2 className="mt-2 text-xl font-semibold">Payment method</h2>
-						<input type="text" name="Voucher" placeholder="Voucher code" className="p-2 rounded-md"/>
-						<select name="delivery" className="p-2 rounded-md" required>
-							<option value="cod">COD</option>
-							<option value="gcash" disabled>GCash</option>
-							<option value="paypal" disabled>Paypal</option>
-						</select>
-						
-						<input type="submit" value="Place order" className="p-3 mt-2 bg-blue-500 text-gray-50 dark:bg-dark-button dark:hover:bg-button-hover rounded-md hover:bg-blue-400 transition delay-100 cursor-pointer"/>
+							<h2 className="mt-2 text-xl font-semibold">Payment method</h2>
+							<input type="text" name="Voucher" placeholder="Voucher code" className="p-2 rounded-md"/>
+							<select name="delivery" className="p-2 rounded-md" required>
+								<option value="cod">COD</option>
+								<option value="gcash" disabled>GCash</option>
+								<option value="paypal" disabled>Paypal</option>
+							</select>
+							
+							<input type="submit" value="Place order" className="p-3 mt-2 bg-blue-500 text-gray-50 dark:bg-dark-button dark:hover:bg-button-hover rounded-md hover:bg-blue-400 transition delay-100 cursor-pointer"/>
+						</div>
 					</form>
-					<div className="justify-center md:px-5 md:py-10">
-						<div className="flex flex-col gap-2 p-5 rounded-xl bg-gray-50 shadow-md dark:bg-dark-card text-gray-800 dark:text-gray-50">
+					<div className="">
+						<div className="flex flex-col gap-2 p-5 rounded-md md:bg-gray-50 md:shadow-md md:dark:bg-dark-card text-gray-800 dark:text-gray-50">
 							<p className="text-xl font-semibold">Order summary</p>
 
 							{items?.map((item, idx) =>(
 								<CheckOutItem key={idx} {...item}/>
 							))}
 
-							<div>
+							<div className="py-4 dark:bg-dark-card bg-gray-50 rounded-md">
 								<p className="text-xl md:text-2xl text-center text-yellow-600 dark:text-yellow-300 transition delay-100">TOTAL: {NumberWithSpace(totalPrice)}</p>
 							</div>
 						</div>

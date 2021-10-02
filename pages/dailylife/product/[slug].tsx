5ad5200aa9ce028ac821ddac6efe6645739fa599
kensidiangco <kensidiangco@gmail.com>
2021-10-02 14:56:01 +0800

    Changing variation value

--- pages/dailylife/product/[slug].tsx
+++ pages/dailylife/product/[slug].tsx
@@ -166,7 +166,7 @@ export default function Product({ product }) {
 											<option 
 												key={idx} 
 												id={data.id} 
-												value={data.slug}
+												value={data.variant_name}
 											>
 												{data.variant_name}
 											</option>
